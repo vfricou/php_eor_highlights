5604fcc360a1b2434d8915052047e0aa2291f448
Vincent FRICOU <vincent@fricouv.eu>
2021-03-03 12:04:13 +0100

    updt: add db connection timeout and error rising

--- include/functions.php
+++ include/functions.php
@@ -6,15 +6,17 @@
     global $db_host;
     global $db_user;
     global $db_pass;
+    global $db_timeout;
     global $mysql_connection;
-    $mysql_connection = new mysqli(
+    $mysql_connection = mysqli_init() or die ("Failed to create mysqli object");
+    $mysql_connection->options(MYSQLI_OPT_CONNECT_TIMEOUT, $db_timeout) or die ('MySQLi Fail to set connect timeout');
+    $mysql_connection->options(MYSQLI_OPT_READ_TIMEOUT, $db_timeout) or die ('MySQLi fail to set read timeout');
+    $mysql_connection->real_connect(
       $db_host,
       $db_user,
       $db_pass
-    );
-    if (!$mysql_connection) {
-      die("Connection failed: " . mysqli_connect_error());
-    }
+    ) or die ('Fail to connect to database: '.mysqli_connect_error());
+
     $mysql_connection->select_db($db_database);
   }
 ?>
